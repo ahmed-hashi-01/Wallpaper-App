4bd81e77ac0c2a78df97ee06c7ef11e5fd9646ba
ahmed hashi <ahmedhashihassan@gmail.com>
2022-07-27 23:04:31 +0300

    aaa

--- app/src/main/java/com/example/wallpaperapp/Adapter.java
+++ app/src/main/java/com/example/wallpaperapp/Adapter.java
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-
+    private String nonse;
     Context context;
     ArrayList<Imagemodel> wallpaperlist;
 
@@ -42,4 +42,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             super(itemView);
         }
     }
+
+    //aaaaaaaaaaaaaaaa
+
+
+
+
 }
