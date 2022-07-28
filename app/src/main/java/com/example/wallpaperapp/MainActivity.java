7189db9e546d265bef17d82a6f667f0654892661
logark12 <ahmedhashihassan@gmail.com>
2022-07-28 10:35:29 +0300

    close to done

--- app/src/main/java/com/example/wallpaperapp/MainActivity.java
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.wallpaperapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Imagemodel> modelClassList;
+    private RecyclerView recyclerView;
+    Adapter adapter;
+    CardView mnature,mbus,mcar,mtrain,mtrending;
+    EditText editText;
+    ImageButton search;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        recyclerView= findViewById(R.id.recyclerview);
+        mnature = findViewById(R.id.nature);
+        mbus = findViewById(R.id.bus);
+        mcar  = findViewById(R.id.car);
+        mtrain = findViewById(R.id.train);
+        mtrending = findViewById(R.id.trending);
+        editText = findViewById(R.id.edittext);
+        search = findViewById(R.id.serach);
+
+        modelClassList = new ArrayList<>();
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setHasFixedSize(true);
+        adapter = new Adapter(getApplicationContext(),modelClassList);
+        recyclerView.setAdapter(adapter);
+        findphotos();
+        
+        mnature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = "nature";
+                getsearchimage(query);
+            }
+        });
+    }
+
+    private void getsearchimage(String query) {
+    }
+
+    private void findphotos() {
     }
 }
